8955ee7eca8b757e80ab825ca3df3842418dcfe1
SavioVarsalle <savio.varsalle@live.com>
2024-02-24 19:30:19 -0300

    Correção na rotina prestador get

--- src/EndPoints/Tomador.php
+++ src/EndPoints/Tomador.php
@@ -14,24 +14,36 @@ class Tomador
 
     private $token;
 
+    private $cnpjCpf;
+
     public function __construct(array $data)
     {
         $this->service      = new GissOnline;
         $this->codMunicipio = env('GISS_COD_MUNICIPIO_PRESTADOR');
         $this->idEmpresa    = data_get($data, 'idEmpresa', '') ;
         $this->token        = data_get($data, 'token', '');
+        $this->cnpjCpf = data_get($data, 'cnpjCpf', null);
     }
 
     public function get()
     {
-        $response = $this->service->api->request('GET', "https://gissv2-{$this->codMunicipio}.giss.com.br/service-empresa/api/empresa/{$this->codMunicipio}/{$this->idEmpresa}", [
+        $response = $this->service->api->request('GET', "https://gissv2-{$this->codMunicipio}.giss.com.br/service-empresa/api/cliente-fornecedor/tomador/{$this->codMunicipio}/{$this->idEmpresa}/tipo/1/cliente/2024-02-23?parametro={$this->cnpjCpf}", [
             'headers' => [
                 'Accept'        => 'application/json, text/plain, */*',
                 'Accept-Encoding' => 'gzip, deflate, br, zstd',
                 'Authorization' => 'Bearer ' . $this->token
             ],
         ]);
-
+        $dadosBasicos = json_decode($response->getBody()->getContents());
+        
+        $response = $this->service->api->request('GET', "https://gissv2-{$this->codMunicipio}.giss.com.br/service-empresa/api/empresa/endereco/{$this->codMunicipio}/{$dadosBasicos->conteudo[0]->id}", [
+            'headers' => [
+                'Accept'        => 'application/json, text/plain, */*',
+                'Accept-Encoding' => 'gzip, deflate, br, zstd',
+                'Authorization' => 'Bearer ' . $this->token
+            ],
+        ]);
+        
         return json_decode($response->getBody()->getContents());
     }
 }
